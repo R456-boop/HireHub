0e1b842537964740bb2692d1576dea5fd992a42a
Rhea_r <riyarai1911@gmail.com>
2026-08-18 09:56:32 +0530

    Update axios configuration

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8000",
-    withCredentials: true,
+    baseURL: "https://hirehub-backend-fqbj.onrender.com",
+    withCredentials: true
 });
-
 export default axiosInstance;
+
+
